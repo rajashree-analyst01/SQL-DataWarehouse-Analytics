d932646bc066710b2d3d7c3ed95c991e9ab17389
rajashree-analyst01 <rayrajashree19@gmail.com>
2026-05-07 20:28:04 +0530

    Update gold-data-load.sql

--- Scripts/Gold Layer/gold-data-load.sql
+++ Scripts/Gold Layer/gold-data-load.sql
@@ -4,8 +4,7 @@ DDL Script: Create Gold Views
 ===============================================================================
 Script Purpose:
     This script creates views for the Gold layer in the data warehouse. 
-    The Gold layer represents the final dimension and fact tables (Star Schema)
-
+    The Gold layer represents the final dimension and fact tables (Star Schema).
     Each view performs transformations and combines data from the Silver layer 
     to produce a clean, enriched, and business-ready dataset.
 
@@ -81,51 +80,3 @@ LEFT JOIN gold.dim_products pr
 LEFT JOIN gold.dim_customers cu
     ON sd.sls_cust_id = cu.customer_id;
 GO
-
-
-/*
-===============================================================================
-Quality Checks
-Purpose:
-    - Performs quality checks to validate the integrity, consistency, 
-    and accuracy of the Gold Layer. 
-    These checks ensure:
-    - Uniqueness of surrogate keys in dimension tables.
-    - Referential integrity between fact and dimension tables.
-    - Validation of relationships in the data model for analytical purposes.
-===============================================================================
-*/
-
-
--- Checking 'gold.dim_customers'
--- Check for Uniqueness of Customer Key in gold.dim_customers
-
-SELECT 
-    customer_key,
-    COUNT(*) AS duplicate_count
-FROM gold.dim_customers
-GROUP BY customer_key
-HAVING COUNT(*) > 1;
-
-
--- Checking 'gold.product_key'
--- Check for Uniqueness of Product Key in gold.dim_products
-
-SELECT 
-    product_key,
-    COUNT(*) AS duplicate_count
-FROM gold.dim_products
-GROUP BY product_key
-HAVING COUNT(*) > 1;
-
-
--- Checking 'gold.fact_sales'
--- Check the data model connectivity between fact and dimensions
-
-SELECT * 
-FROM gold.fact_sales f
-LEFT JOIN gold.dim_customers c
-ON c.customer_key = f.customer_key
-LEFT JOIN gold.dim_products p
-ON p.product_key = f.product_key
-WHERE p.product_key IS NULL OR c.customer_key IS NULL  
