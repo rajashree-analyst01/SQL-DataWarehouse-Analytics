a542396a139e62c8093cba3daf5e2dbfddc1bfa7
rajashree-analyst01 <rayrajashree19@gmail.com>
2026-06-11 11:27:44 +0530

    Add files via upload

--- Scripts/02_Bronze_Layer/bronze_ddl.sql
+++ Scripts/02_Bronze_Layer/bronze_ddl.sql
@@ -1,28 +1,71 @@
 /*
-=============================================================
-Create Database and Schemas
-=============================================================
-Script Purpose:
-    This script creates a new database named 'DataWarehouse'. Additionally, the script sets up three schemas 
-    within the database: 'bronze', 'silver', and 'gold'.
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Purpose:
+    This script creates tables in the 'bronze' schema.
+	Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
 */
 
-USE master;
+
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
+);
 GO
 
--- Create the 'DataWarehouse' database
-CREATE DATABASE DataWarehouse;
+
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
 GO
 
-USE DataWarehouse;
+
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
 GO
 
--- Create Schemas
-CREATE SCHEMA bronze;
+
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
 GO
 
-CREATE SCHEMA silver;
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
 GO
 
-CREATE SCHEMA gold;
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
 GO
+
